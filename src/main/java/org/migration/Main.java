a8ac401090f21ede7e2c436dc89a55301ee1e9fc
victoria_rusalovskaya <vika.rusalovskaya@gmail.com>
2026-04-02 16:14:33 +0200

    add input from console

--- src/main/java/org/migration/Main.java
+++ src/main/java/org/migration/Main.java
@@ -5,6 +5,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     private static final Logger log = LogManager.getLogger(Main.class);
@@ -12,7 +13,25 @@ public class Main {
     public static void main(String[] args) {
         MigrationService service = new MigrationService(new MigrationRepository());
         try {
-            service.migrate();
+            Scanner in = new Scanner(System.in);
+            System.out.println("""
+                    Custom migration tool
+                    Please specify the required action:
+                    /start - initiating migration process
+                    /end - terminate program""");
+            while (true) {
+                String command = in.nextLine();
+                if (command.equals("/start")) {
+                    System.out.println("Starting migration tool...");
+                    service.migrate();
+                    break;
+                } else if (command.equals("/end")) {
+                    break;
+                } else  {
+                    System.out.println("Invalid command. Try again.");
+                }
+            }
+
         } catch (SQLException | IOException | InterruptedException e) {
             log.error(e);
             System.out.println(e.getMessage());
